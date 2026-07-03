0436d199b907427dc0e3ba61ea56405517010e76
jeerawitch <jeerawitch2115@gmail.com>
2026-07-03 23:07:28 +0700

    added content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
+import MainContent from "./components/MainContent";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
@@ -9,6 +9,12 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia illum
+          incidunt, officia tenetur veniam porro doloribus atque laudantium
+          ratione voluptatum iure sapiente esse libero dolorum quaerat
+          perferendis minima necessitatibus rerum.
+        </p>
       </header>
       <MainContent />
     </>
